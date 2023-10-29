9ff71d2673b7463984ded6703ca193f509f460ff
Zilong Liu <lzlong@amazon.com>
2023-10-29 12:00:37 +0800

    basic use of xhr-backend

--- src/MyAlert.tsx
+++ src/MyAlert.tsx
@@ -3,8 +3,7 @@ import {Alert} from 'antd';
 import { withTranslation, WithTranslation } from 'react-i18next';
 
 function MyAlert({t}: WithTranslation){
-    console.log(t("Welcome to React"))
-    return <Alert message={t('Welcome to React')} type="success"/>;
+    return <Alert message={t('welcome')} type="success"/>;
 }
 
 export default withTranslation()(MyAlert);
